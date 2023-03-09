598e7bac5abacec28265c1cecbc7d782b338eb5b
danilobatson <djbatson19@gmail.com>
2023-03-08 21:29:56 -0800

    adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'no-unused-vars': 'warn'
+  }
+}
